4155a4bf99a289fe8e8d766e8a1e18a8e868183e
Randy Chong <randychong27@yahoo.com>
2021-06-15 15:50:39 -0500

    moved navbar from home to navbar component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,40 +1,9 @@
 import React from "react";
-import M4 from "../assets/bmw-m4.png";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Pag</h1>
-
-      <nav
-        class="navbar has-shadow is-black"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src={M4}></img>
-          </a>
-
-          <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
-        <div class="navbar-menu is-active">
-          <a class="navbar-item">Home</a>
-          <a class="navbar-item">Garage</a>
-          <a class="navbar-item">Account</a>
-        </div>
-      </nav>
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,38 @@
 import React from "react";
+import M4 from "../assets/bmw-m4.png";
 
 export default function Navbar() {
   return (
     <div>
-      <h1>Nav</h1>
+      <nav
+        class="navbar has-shadow is-black"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src={M4}></img>
+          </a>
+
+          <a
+            role="button"
+            class="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div class="navbar-menu is-active">
+          <a class="navbar-item">Home</a>
+          <a class="navbar-item">Garage</a>
+          <a class="navbar-item">Account</a>
+        </div>
+      </nav>
     </div>
   );
 }
