7aef49af36a4d6419837b49b42a981f701c08247
Randy Chong <randychong27@yahoo.com>
2021-06-16 09:07:12 -0500

    added react bootstrap navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Nav from "./Navbar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Pag</h1>
+      <Nav />
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,22 @@
 import React from "react";
-import M4 from "../assets/bmw-m4.png";
+import { Navbar as RBNavbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Garage from "./Garage";
 
 export default function Navbar() {
   return (
     <div>
-      <nav
-        class="navbar has-shadow is-black"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src={M4}></img>
-          </a>
-
-          <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
-        <div class="navbar-menu is-active">
-          <a class="navbar-item">Home</a>
-          <a class="navbar-item">Garage</a>
-          <a class="navbar-item">Account</a>
-        </div>
-      </nav>
+      <RBNavbar bg="light" expand="lg">
+        <RBNavbar.Brand href="#home">Branding</RBNavbar.Brand>
+        <RBNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <RBNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Garage</Nav.Link>
+          </Nav>
+        </RBNavbar.Collapse>
+      </RBNavbar>
     </div>
   );
 }
