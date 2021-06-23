fc499e7daa027e0c0cddee9640956495e9fc8a7b
Randy Chong <randychong27@yahoo.com>
2021-06-23 17:00:17 -0500

    updated confirmation image to fit screen

--- client/src/components/Confirmation.js
+++ client/src/components/Confirmation.js
@@ -5,7 +5,6 @@ import background from "../assets/background.gif";
 export default function Confirmation() {
   return (
     <div>
-      {/* <div className="confirmationContainer"></div> */}
       <Container>
         <Image src={background} />
       </Container>
